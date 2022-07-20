abe8f37907df85278a746ebc30b8888b9c38e579
bjoern.eschle <bjoern.eschle@emea.merkleinc.com>
2022-07-20 10:04:24 +0200

    Use I18nAuthoringSupport in DefaultLocalizedPropertyNameProvider

--- src/main/java/com/namics/oss/magnolia/powernode/DefaultLocalizedPropertyNameProvider.java
+++ src/main/java/com/namics/oss/magnolia/powernode/DefaultLocalizedPropertyNameProvider.java
@@ -1,6 +1,7 @@
 package com.namics.oss.magnolia.powernode;
 
 import info.magnolia.cms.i18n.I18nContentSupport;
+import info.magnolia.ui.api.i18n.I18NAuthoringSupport;
 
 import javax.inject.Inject;
 import java.util.Locale;
@@ -8,17 +9,21 @@ import java.util.Objects;
 
 public class DefaultLocalizedPropertyNameProvider implements LocalizedPropertyNameProvider {
 	private final I18nContentSupport i18nContentSupport;
+	private final I18NAuthoringSupport i18NAuthoringSupport;
 
 	@Inject
-	public DefaultLocalizedPropertyNameProvider(final I18nContentSupport i18nContentSupport) {
+	public DefaultLocalizedPropertyNameProvider(
+			final I18nContentSupport i18nContentSupport,
+			final I18NAuthoringSupport i18NAuthoringSupport) {
 		this.i18nContentSupport = i18nContentSupport;
+		this.i18NAuthoringSupport = i18NAuthoringSupport;
 	}
 
 	@Override
 	public String getLocalized(final String propertyName, final Locale locale) {
 		final boolean isDefault = Objects.equals(locale, i18nContentSupport.getDefaultLocale());
 		if (!isDefault) {
-			return propertyName + "_" + locale; // same as AbstractI18nContentSupport.getProperty(Node node, String name, Locale locale)
+			return i18NAuthoringSupport.deriveLocalisedPropertyName(propertyName, locale);
 		}
 		return propertyName;
 	}
--- src/test/java/com/namics/oss/magnolia/powernode/AbstractPowerNodeTest.java
+++ src/test/java/com/namics/oss/magnolia/powernode/AbstractPowerNodeTest.java
@@ -2,15 +2,16 @@ package com.namics.oss.magnolia.powernode;
 
 import com.namics.oss.magnolia.AbstractXmlRepositoryTest;
 import info.magnolia.cms.i18n.I18nContentSupport;
-import info.magnolia.module.site.i18n.SiteI18nContentSupport;
 import info.magnolia.repository.RepositoryConstants;
 import info.magnolia.test.ComponentsTestUtil;
+import info.magnolia.ui.api.i18n.I18NAuthoringSupport;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mockito;
 
 import java.util.Locale;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
 
 public abstract class AbstractPowerNodeTest extends AbstractXmlRepositoryTest {
 
@@ -22,12 +23,19 @@ public abstract class AbstractPowerNodeTest extends AbstractXmlRepositoryTest {
 	public void setUp() throws Exception {
 		super.setUp();
 
-		SiteI18nContentSupport mockSiteI18nContentSupport = Mockito.mock(SiteI18nContentSupport.class);
-		Mockito.when(mockSiteI18nContentSupport.toI18NURI(Mockito.anyString())).thenAnswer(invocation -> invocation.getArguments()[0]);
-		Mockito.doReturn(Locale.GERMAN).when(mockSiteI18nContentSupport).getDefaultLocale();
-		ComponentsTestUtil.setInstance(I18nContentSupport.class, mockSiteI18nContentSupport);
+		final I18nContentSupport mockI18nContentSupport = Mockito.mock(I18nContentSupport.class);
+		Mockito.when(mockI18nContentSupport.toI18NURI(Mockito.anyString())).thenAnswer(invocation -> invocation.getArguments()[0]);
+		Mockito.doReturn(Locale.GERMAN).when(mockI18nContentSupport).getDefaultLocale();
+		ComponentsTestUtil.setInstance(I18nContentSupport.class, mockI18nContentSupport);
 
-		this.powerNodeService = new PowerNodeService(new LanguageLocalizedPropertyNameProvider(mockSiteI18nContentSupport));
+		final I18NAuthoringSupport mockI18NAuthoringSupport = Mockito.mock(I18NAuthoringSupport.class);
+		doAnswer(invocationOnMock -> {
+			final String propertyName = invocationOnMock.getArgument(0);
+			final Locale locale = invocationOnMock.getArgument(1);
+			return propertyName + "_" + locale.getLanguage();
+		}).when(mockI18NAuthoringSupport).deriveLocalisedPropertyName(any(), any());
+
+		this.powerNodeService = new PowerNodeService(new DefaultLocalizedPropertyNameProvider(mockI18nContentSupport, mockI18NAuthoringSupport));
 	}
 
 	public String getRepositoryXmlPath() {
