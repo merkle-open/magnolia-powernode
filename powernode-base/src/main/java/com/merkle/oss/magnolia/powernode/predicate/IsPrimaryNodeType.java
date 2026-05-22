31709ef85701b968098107b7b75a4c5be8ec8a05
eschleb <bjoern.eschle@merkle.com>
2026-05-22 07:22:25 +0200

    Implement multiple/any IsPrimaryNodeType

--- powernode-base/src/main/java/com/merkle/oss/magnolia/powernode/predicate/IsPrimaryNodeType.java
+++ powernode-base/src/main/java/com/merkle/oss/magnolia/powernode/predicate/IsPrimaryNodeType.java
@@ -1,19 +1,26 @@
 package com.merkle.oss.magnolia.powernode.predicate;
 
-import com.merkle.oss.magnolia.powernode.RepositoryExceptionDelegator;
-
-import javax.jcr.Node;
+import java.util.Set;
 import java.util.function.Predicate;
 
-public class IsPrimaryNodeType<N extends Node> extends RepositoryExceptionDelegator implements Predicate<N> {
-	private final String nodeType;
+import javax.jcr.Node;
 
-	public IsPrimaryNodeType(final String nodeType) {
-		this.nodeType = nodeType;
+import com.merkle.oss.magnolia.powernode.RepositoryExceptionDelegator;
+
+public class IsPrimaryNodeType<N extends Node> extends RepositoryExceptionDelegator implements Predicate<N> {
+    private final Set<String> nodeTypes;
+
+    public IsPrimaryNodeType(final String... nodeTypes) {
+		this(Set.of(nodeTypes));
 	}
+	public IsPrimaryNodeType(final Set<String> nodeTypes) {
+        this.nodeTypes = nodeTypes;
+    }
 
 	@Override
 	public boolean test(final N node) {
-		return getOrThrow(node::getPrimaryNodeType).isNodeType(nodeType);
+		return nodeTypes.stream().anyMatch(nodeType ->
+				get(node::getPrimaryNodeType).map(primaryNodeType -> primaryNodeType.isNodeType(nodeType)).orElse(false)
+		);
 	}
 }
