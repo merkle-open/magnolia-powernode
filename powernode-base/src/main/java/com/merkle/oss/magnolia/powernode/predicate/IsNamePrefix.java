876688fb9945a72b18850433776a700b81d17bbd
eschleb <bjoern.eschle@merkle.com>
2026-05-22 07:22:14 +0200

    Implement multiple/any IsNamePrefix

--- powernode-base/src/main/java/com/merkle/oss/magnolia/powernode/predicate/IsNamePrefix.java
+++ powernode-base/src/main/java/com/merkle/oss/magnolia/powernode/predicate/IsNamePrefix.java
@@ -3,17 +3,24 @@ package com.merkle.oss.magnolia.powernode.predicate;
 import com.merkle.oss.magnolia.powernode.RepositoryExceptionDelegator;
 
 import javax.jcr.Node;
+
+import java.util.Set;
 import java.util.function.Predicate;
 
 public class IsNamePrefix<N extends Node> extends RepositoryExceptionDelegator implements Predicate<N> {
-	private final String namePrefix;
+	private final Set<String> namePrefixes;
 
-	public IsNamePrefix(final String namePrefix) {
-		this.namePrefix = namePrefix;
+	public IsNamePrefix(final String... namePrefixes) {
+		this(Set.of(namePrefixes));
+	}
+	public IsNamePrefix(final Set<String> namePrefixes) {
+		this.namePrefixes = namePrefixes;
 	}
 
 	@Override
 	public boolean test(final N node) {
-		return getOrThrow(node::getName).startsWith(namePrefix);
+		return namePrefixes.stream().anyMatch(namePrefix ->
+				get(node::getName).map(name -> name.startsWith(namePrefix)).orElse(false)
+		);
 	}
 }
