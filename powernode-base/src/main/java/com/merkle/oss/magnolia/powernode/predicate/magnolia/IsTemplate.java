2157d9bef7dbc67c6c089c0b8b95c2fb14e0de8f
eschleb <bjoern.eschle@merkle.com>
2026-05-22 07:01:04 +0200

    Improve IsTemplate predicate performance

--- powernode-base/src/main/java/com/merkle/oss/magnolia/powernode/predicate/magnolia/IsTemplate.java
+++ powernode-base/src/main/java/com/merkle/oss/magnolia/powernode/predicate/magnolia/IsTemplate.java
@@ -1,17 +1,24 @@
 package com.merkle.oss.magnolia.powernode.predicate.magnolia;
 
-import com.merkle.oss.magnolia.powernode.AbstractPowerNode;
-import com.merkle.oss.magnolia.powernode.RepositoryExceptionDelegator;
-import com.merkle.oss.magnolia.powernode.ValueConverter;
-import com.merkle.oss.magnolia.powernode.predicate.HasPropertyValue;
 import info.magnolia.jcr.util.NodeTypes;
 
-import javax.jcr.Node;
-import java.util.Objects;
+import java.util.Set;
 import java.util.function.Predicate;
 
-public class IsTemplate<N extends AbstractPowerNode<N>> extends HasPropertyValue<N, String> {
-	public IsTemplate(final String templateId) {
-		super(NodeTypes.Renderable.TEMPLATE, ValueConverter::getString, templateId);
+import com.merkle.oss.magnolia.powernode.AbstractPowerNode;
+
+public class IsTemplate<N extends AbstractPowerNode<N>> implements Predicate<N> {
+	private final Set<String> templateIds;
+
+	public IsTemplate(final String... templateIds) {
+		this(Set.of(templateIds));
+	}
+	public IsTemplate(final Set<String> templateIds) {
+        this.templateIds = templateIds;
+    }
+
+	@Override
+	public boolean test(final N node) {
+		return node.get(NodeTypes.Renderable::getTemplate).map(templateIds::contains).orElse(false);
 	}
 }
